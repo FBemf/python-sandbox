0546437ffea3fecdbcff22c50b0429d3cb9c1469
Felipe Bemfica <felipe.bemfica@arcticwolf.com>
2019-11-28 12:51:11 -0500

    got rid of jquery again

--- server/static/script.js
+++ server/static/script.js
@@ -2,72 +2,71 @@
 
     // Hide the output box
     window.onload = function () {
-        $("#output").hide();
-    }
+        document.querySelector("#output").style.display = "none"
 
-    // Tabs and backspaces
-    $(document).delegate("#code", "keydown", function(e) {
-        let keyCode = e.keyCode || e.which;
-    
-        console.log(keyCode);
-        if (keyCode == 8) {
-            if (this.selectionStart == null
-                    || this.selectionStart == this.selectionEnd) {
-                let cursorPosition = this.selectionEnd;
-                let beforeCursor = $(this).val() .substring(0, cursorPosition);
-                let numSpaces = 0;
-                let i = beforeCursor.length - 1;
-                while (beforeCursor[i] == " ") {
-                    numSpaces += 1;
-                    i -= 1;
+        // Tabs and backspaces
+        document.querySelector("#code").addEventListener("keydown", function(e) {
+            let keyCode = e.keyCode || e.which;
+        
+            if (keyCode == 8) {
+                if (this.selectionStart == null
+                        || this.selectionStart == this.selectionEnd) {
+                    let cursorPosition = this.selectionEnd;
+                    let beforeCursor = this.value.substring(0, cursorPosition);
+                    let numSpaces = 0;
+                    let i = beforeCursor.length - 1;
+                    while (beforeCursor[i] == " ") {
+                        numSpaces += 1;
+                        i -= 1;
+                    }
+                    if (numSpaces > 0 && numSpaces % 4 == 0) {
+                        e.preventDefault();
+                        this.value = this.value.substring(0, cursorPosition - 4)
+                            + this.value.substring(cursorPosition);
+                        this.selectionEnd = cursorPosition - 4;
+                        this.selectionStart = this.selectionEnd;
+                    }
                 }
-                if (numSpaces > 0 && numSpaces % 4 == 0) {
-                    e.preventDefault();
-                    $(this).val($(this).val().substring(0, cursorPosition - 4)
-                        + $(this).val().substring(cursorPosition));
-                    this.selectionEnd = cursorPosition - 4;
-                    this.selectionStart = this.selectionEnd;
-                }
+            } else if (keyCode == 9) {
+                e.preventDefault();
+                let start = this.selectionStart;
+                let end = this.selectionEnd;
+                let insertString = "    "
+            
+                // set textarea value to: text before caret + tab + text after caret
+                this.value = this.value.substring(0, start)
+                            + insertString
+                            + this.value.substring(end);
+            
+                // put caret at right position again
+                this.selectionEnd = start + insertString.length;
+                this.selectionStart = this.selectionEnd;
             }
-        } else if (keyCode == 9) {
-            e.preventDefault();
-            let start = this.selectionStart;
-            let end = this.selectionEnd;
-            let insertString = "    "
-        
-            // set textarea value to: text before caret + tab + text after caret
-            $(this).val($(this).val().substring(0, start)
-                        + insertString
-                        + $(this).val().substring(end));
-        
-            // put caret at right position again
-            this.selectionEnd = start + insertString.length;
-            this.selectionStart = this.selectionEnd;
-        }
-    });
+        });
+    }
 })();
 
 // Execute the code in the box
 function execute() {
-    $("#output").html("<em>LOADING...</em>");
-    $("#output").show();
+    document.querySelector("#output").innerHTML = "<em>LOADING...</em>";
+    document.querySelector("#output").style.display = "block";
 
     data = JSON.stringify({
-        code: $("#code").val(),
+        code: document.querySelector("#code").value,
     });
-
-    $.ajax({
-        type: "POST",
-        url: "/run",
+    fetch("/run", {
+        method: "POST",
         headers: {
             "Content-Type": "application/json; charset=UTF-8",
         },
-        data: data,
-        success: function(result) {
-            $("#output").html(result);
-        },
-        error: function(xhr) {
-            $("#output").html("<em>SERVER ERROR</em>");
-        },
+        body: data,
+    }).then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            return "<em>SERVER ERROR</em>";
+        }
+    }).then(result => {
+        document.querySelector("#output").innerHTML = result;
     });
 }
