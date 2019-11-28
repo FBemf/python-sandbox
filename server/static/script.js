cc01cad69e06250712daa174bd234d8d2f0015de
Felipe Bemfica <felipe.bemfica@arcticwolf.com>
2019-11-28 15:23:55 -0500

    notifications

--- server/static/script.js
+++ server/static/script.js
@@ -1,50 +1,44 @@
-(function() {
+window.onload = function () {
+    // Tabs and backspaces
+    document.querySelector("#code").addEventListener("keydown", function(e) {
+        let keyCode = e.keyCode || e.which;
 
-    // Hide the output box
-    window.onload = function () {
-        document.querySelector("#output-box").style.display = "none"
-
-        // Tabs and backspaces
-        document.querySelector("#code").addEventListener("keydown", function(e) {
-            let keyCode = e.keyCode || e.which;
-        
-            if (keyCode == 8) {
-                if (this.selectionStart == null
-                        || this.selectionStart == this.selectionEnd) {
-                    let cursorPosition = this.selectionEnd;
-                    let beforeCursor = this.value.substring(0, cursorPosition);
-                    let numSpaces = 0;
-                    let i = beforeCursor.length - 1;
-                    while (beforeCursor[i] == " ") {
-                        numSpaces += 1;
-                        i -= 1;
-                    }
-                    if (numSpaces > 0 && numSpaces % 4 == 0) {
-                        e.preventDefault();
-                        this.value = this.value.substring(0, cursorPosition - 4)
-                            + this.value.substring(cursorPosition);
-                        this.selectionEnd = cursorPosition - 4;
-                        this.selectionStart = this.selectionEnd;
-                    }
+        if (keyCode == 8) {
+            if (this.selectionStart == null
+                    || this.selectionStart == this.selectionEnd) {
+                let cursorPosition = this.selectionEnd;
+                let beforeCursor = this.value.substring(0, cursorPosition);
+                let numSpaces = 0;
+                let i = beforeCursor.length - 1;
+                while (beforeCursor[i] == " ") {
+                    numSpaces += 1;
+                    i -= 1;
                 }
-            } else if (keyCode == 9) {
-                e.preventDefault();
-                let start = this.selectionStart;
-                let end = this.selectionEnd;
-                let insertString = "    "
-            
-                // set textarea value to: text before caret + tab + text after caret
-                this.value = this.value.substring(0, start)
-                            + insertString
-                            + this.value.substring(end);
-            
-                // put caret at right position again
-                this.selectionEnd = start + insertString.length;
-                this.selectionStart = this.selectionEnd;
+                if (numSpaces > 0 && numSpaces % 4 == 0) {
+                    e.preventDefault();
+                    this.value = this.value.substring(0, cursorPosition - 4)
+                        + this.value.substring(cursorPosition);
+                    this.selectionEnd = cursorPosition - 4;
+                    this.selectionStart = this.selectionEnd;
+                }
             }
-        });
-    }
-})();
+        } else if (keyCode == 9) {
+            e.preventDefault();
+            let start = this.selectionStart;
+            let end = this.selectionEnd;
+            let insertString = "    "
+
+            // set textarea value to: text before caret + tab + text after caret
+            this.value = this.value.substring(0, start)
+                        + insertString
+                        + this.value.substring(end);
+
+            // put caret at right position again
+            this.selectionEnd = start + insertString.length;
+            this.selectionStart = this.selectionEnd;
+        }
+    });
+}
 
 // Execute the code in the box
 function execute() {
@@ -62,23 +56,46 @@ function execute() {
     }).then(response => {
         if (response.ok) {
             response.text().then(result => {
-                parsed = JSON.parse(result);
-                document.querySelector("#output").innerHTML = parsed.text;
+                let parsed = JSON.parse(result);
+                let notifs = document.querySelector("#notifications");
+                let status = "";
+                let title = "";
                 if (parsed.status == "success") {
-                    document.querySelector("#output-box").classList.add("is-success");
+                    title = "Python Output"
+                    status = "is-success";
                 } else if (parsed.status == "error") {
-                    document.querySelector("#output-box").classList.add("is-warning");
+                    title = "Python Error"
+                    status = "is-warning";
                 }
+                notifs.insertBefore(notification(title, parsed.text, status), notifs.firstChild);
             })
         } else {
-            document.querySelector("#output").innerHTML = "SERVER ERROR";
-            document.querySelector("#output-box").classList.add("is-error");
+            let notifs = document.querySelector("#notifications");
+            notifs.insertBefore(notification("Error!", "SERVER ERROR", "is-error"), notifs.firstChild);
         }
         document.querySelector("#execute").classList.remove("is-loading");
-        box = document.querySelector("#output-box");
-        box.classList.remove("is-success");
-        box.classList.remove("is-warning");
-        box.classList.remove("is-error");
-        box.style.display = "block";
     });
+}
+
+function notification(title, msg, type) {
+    let newNotif = document.createElement("article");
+    newNotif.classList.add("message");
+    newNotif.classList.add("is-small");
+    newNotif.classList.add(type);
+    let header = document.createElement("div");
+    header.classList.add("message-header");
+    header.innerHTML = title;
+    let del = document.createElement("button");
+    del.classList.add("delete");
+    del.addEventListener("click", function () {
+        newNotif.parentNode.removeChild(newNotif);
+    });
+    header.appendChild(del);
+    let body = document.createElement("div");
+    body.classList.add("message-body");
+    body.classList.add("is-family-code");
+    body.innerHTML = msg;
+    newNotif.appendChild(header);
+    newNotif.appendChild(body);
+    return newNotif
 }
